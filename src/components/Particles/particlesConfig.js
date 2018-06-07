33a56c17d221569c9d3677548eace235ef23ed8c
Adam King <adk486@gmail.com>
2018-06-06 21:39:53 -0700

    update particles.js to reduce CPU usage and save the planet

--- src/components/Particles/particlesConfig.js
+++ src/components/Particles/particlesConfig.js
@@ -1,97 +1,34 @@
 const config = {
 	"particles": {
 		"number": {
-			"value": 25,
+			"value": 20,
 			"density": {
 				"enable": true,
-				"value_area": 300
+				"value_area": 100
 			}
 		},
 		"color": {
 			"value": "#ffffff"
 		},
 		"shape": {
-			"type": "circle"
+			"type": "polygon",
+			"polygon": {
+				"nb_sides": 6
+			}
 		},
 		"opacity": {
 			"value": 0.5,
-			"random": true,
-			"anim": {
-				"enable": true,
-				"speed": 1,
-				"opacity_min": 0.1,
-				"sync": false
-			}
+			"random": true
 		},
 		"size": {
-			"value": 2,
-			"random": true,
-			"anim": {
-				"enable": true,
-				"speed": 4.87155337598649,
-				"size_min": 0.1,
-				"sync": false
-			}
+			"value": 1.5,
+			"random": true
 		},
 		"line_linked": {
-			"enable": true,
-			"distance": 64,
-			"color": "#ffffff",
-			"opacity": 0.05,
-			"width": 1
+			"enable": false,
 		},
 		"move": {
-			"enable": true,
-			"speed": 0,
-			"direction": "none",
-			"random": true,
-			"straight": false,
-			"out_mode": "out",
-			"bounce": false,
-			"attract": {
-				"enable": false,
-				"rotateX": 600,
-				"rotateY": 1200
-			}
-		}
-	},
-	"interactivity": {
-		"detect_on": "canvas",
-		"events": {
-			"onhover": {
-				"enable": false,
-				"mode": "push"
-			},
-			"onclick": {
-				"enable": false,
-				"mode": "push"
-			},
-			"resize": false
-		},
-		"modes": {
-			"grab": {
-				"distance": 400,
-				"line_linked": {
-					"opacity": 1
-				}
-			},
-			"bubble": {
-				"distance": 400,
-				"size": 40,
-				"duration": 2,
-				"opacity": 8,
-				"speed": 3
-			},
-			"repulse": {
-				"distance": 200,
-				"duration": 0.4
-			},
-			"push": {
-				"particles_nb": 4
-			},
-			"remove": {
-				"particles_nb": 2
-			}
+			"enable": false
 		}
 	},
 	"retina_detect": true
